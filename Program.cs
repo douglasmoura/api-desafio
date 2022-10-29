4902c5cf11c472e4fd4674c7a7c197b433ca4101
douglasmoura <douglasmoura2@outlook.com>
2022-10-29 15:48:32 -0300

    concluido

--- Program.cs
+++ Program.cs
@@ -2,12 +2,16 @@ using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 using TrilhaApiDesafio.Context;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//builder.Services.AddDbContext<OrganizadorContext>(options =>
+// options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
-
+    options.UseInMemoryDatabase("teste"));
+    
+builder.Services.AddScoped<OrganizadorContext,OrganizadorContext>();
 builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
